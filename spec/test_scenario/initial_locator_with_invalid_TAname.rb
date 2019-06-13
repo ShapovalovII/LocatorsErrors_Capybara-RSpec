73234b3cb4f5ac9ef1195a015d070b90225ca7dc
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-06-13 14:14:33 +0300

    New error message on Initial locator with invalid TA name

--- spec/test_scenario/initial_locator_with_invalid_TAname.rb
+++ spec/test_scenario/initial_locator_with_invalid_TAname.rb
@@ -5,8 +5,8 @@ feature 'Verification of information output the locators errors.' do
   scenario 'Initial locator with invalid TA name' do
     visit 'https://accounts.google.com'
 
-    withTALocator = ta('ta!Name', "//span[@class='RveJvd snByac']");
-    expError = 'TrueAutomation locator name contains unsupported characters. Please make sure to use only letters, numbers, colon and underscore symbols in locator names'
+    withTALocator = ta('ta-Name', "//span[@class='RveJvd snByac']")
+    expError = 'TrueAutomation locator ta-Name contains unsupported characters. Please make sure to use only letters, numbers, colon and underscore symbols in locator names.'
 
     checkErrorMessageTA(withTALocator, expError)
   end
